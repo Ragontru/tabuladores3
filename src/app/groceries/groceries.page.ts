d88635aa65004a87665ba60453b87795ee1c5dbc
Ragontru <ragontru25@gmail.com>
2021-03-29 14:41:36 +0200

    tabuladores 3 acabado

---
+++ src/app/groceries/groceries.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groceries',
+  templateUrl: './groceries.page.html',
+  styleUrls: ['./groceries.page.scss'],
+})
+export class GroceriesPage implements OnInit {
+
+  public groceries: any;
+
+  constructor() {
+    this.groceries = [
+      'Bread',
+      'Milk',
+      'Cheese',
+      'Snacks',
+      'Apples',
+      'Bananas',
+      'Peanut Butter',
+      'Chocolate',
+      'Avocada',
+      'Vegemite',
+      'Muffins',
+      'Paper towels'
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
